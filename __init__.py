b3c59d33b2a276ad5a292ca8514f8cdc40effd74
oliveralonzo <oliveralonzof@gmail.com>
2018-04-04 23:15:16 -0500

    add import

--- __init__.py
+++ __init__.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
 
+from os.path import dirname
+
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
